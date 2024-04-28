a69d9cb3d834761f8542faf71babb35325fdf181
vel-3633f <vel3633fu@gmail.com>
2024-04-28 12:35:23 +0900

    ローディングをテストで追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
+import { useEffect, useState } from "react";
 import About from "./components/About";
 import First from "./components/First";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Loading from "./components/Loading";
 import Member from "./components/Member";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="flex justify-center items-center flex-col">
-      <Header />
-      <First />
-      <About />
-      <Member />
-      <Footer />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+          <First />
+          <About />
+          <Member />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
