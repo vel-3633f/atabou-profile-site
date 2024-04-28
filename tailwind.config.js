b9eb6596707958c20e81be3a45aae7e405a937a6
vel-3633f <vel3633fu@gmail.com>
2024-04-28 11:35:55 +0900

    静的な箇所は完成

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: "#F9EDDC",
-      secondary: "#e4e4e4",
+    extend: {
+      colors: {
+        primary: "#F9EDDC",
+        secondary: "#e4e4e4",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
